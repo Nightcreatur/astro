f191c0418902b05c1a35e3250ecf93fb90a95767
Nightcreatur <sandeeptimilsena333@gmail.com>
2024-11-13 19:41:15 +0545

    2

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SplashScreen from "../components/SplashScreen";
 import LoginPage from "../components/LoginPage/Login1";
-import Profile from "../components/Profile/Profile";
+
 import ProfileSetup from "../components/PofileSetup/ProfileSetup";
 import ZodiacWheel from "../components/Zodiac/ZodiacWheel";
 import ProfileGrid from "../components/HomePage/ProfileGrid";
-import Message from '../components/Message/Message'
-import HomePage from "../components/HomePage/Homepage";
+
+
 
 
 
@@ -20,11 +20,11 @@ const Webroutes = () => {
         <Route path="/l2" element={<ZodiacWheel />} />
         <Route path="/l3" element={<ProfileSetup />} />
         <Route path="/test" element={<ProfileGrid />} />
-        <Route element={<HomePage />}>
+        {/* <Route element={<HomePage />}>
           <Route path="/home" element={<ProfileGrid />} />
           <Route path="/message" element={<Message />} />
           <Route path="/profile" element={<Profile />} />
-        </Route>
+        </Route> */}
 
       </Routes>
     </Router>
